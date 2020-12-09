d33c15f9265cd9ab93611cefc8aeea5bc185a751
Andrew Carolan <andycarolan9@gmail.com>
2020-12-09 22:02:11 +0000

    Updated UI

--- Assets/Scripts/Unity_Scripts/AudioCommunicator.cs
+++ Assets/Scripts/Unity_Scripts/AudioCommunicator.cs
@@ -5,14 +5,27 @@ using UnityEngine.UI;
 
 public class AudioCommunicator : MonoBehaviour
 {
+    #region UI
     public Slider frequencySlider;
+    public Text freqMinValue;
+    public Text freqMaxValue;
+
     public Slider ampSlider;
+    public Text ampMinValue;
+    public Text ampMaxValue;
+    #endregion
 
     private CsoundUnity csound;
 
     private void Awake()
     {
         csound = GetComponent<CsoundUnity>();
+
+        freqMinValue.text = frequencySlider.minValue.ToString();
+        freqMaxValue.text = frequencySlider.maxValue.ToString();
+
+        ampMinValue.text = ampSlider.minValue.ToString();
+        ampMaxValue.text = ampSlider.maxValue.ToString();
     }
 
     // Update is called once per frame
