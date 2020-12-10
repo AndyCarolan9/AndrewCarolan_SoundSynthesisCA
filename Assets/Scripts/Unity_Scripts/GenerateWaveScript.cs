85ce6809addc97eec015dbb94a96c052e619f425
Andrew Carolan <andycarolan9@gmail.com>
2020-12-10 13:42:25 +0000

    Created UI Line Renderer

--- Assets/Scripts/Unity_Scripts/GenerateWaveScript.cs
+++ Assets/Scripts/Unity_Scripts/GenerateWaveScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 /*
@@ -9,34 +10,77 @@ using UnityEngine;
 public class GenerateWaveScript : MonoBehaviour
 {
     AudioSource source;
-    CsoundUnity csound;
-
-    [SerializeField]
-    public float amp = 5;
-
-    [SerializeField]
-    public float frequency = 10;
 
     public float[] samples = new float[512];
+
+    public float[] freqBand;
+
+    public Image[] freqBand_Images;
+
+    public float startScale = 1;
+    public float scaleMultiplier = 10;
 
     // Start is called before the first frame update
     void Start()
     {
         source = GetComponent<AudioSource>();
-        csound = GetComponent<CsoundUnity>();
+
+        freqBand = new float[freqBand_Images.Length];
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetSpectrumData();
+        //GetSpectrumData();
 
-        csound.setChannel("frequency", frequency);
-        csound.setChannel("amplitude", amp);
+        //MakeFrequencyBands();
+
+        GetClipData();
+    }
+
+    void GetClipData()
+    {
+        source.GetOutputData(samples, 1);
     }
 
     void GetSpectrumData()
     {
         source.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+
+        for(int i = 0; i < freqBand.Length; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if(i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for(int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            freqBand[i] = average * 10;
+        }
+
+        for (int k = 0; k < freqBand_Images.Length; k++)
+        {
+            freqBand_Images[k].transform.localScale = new Vector3(
+                freqBand_Images[k].transform.localScale.x,
+                (freqBand[k] * scaleMultiplier) + startScale,
+                freqBand_Images[k].transform.localScale.x);
+
+            Debug.Log(freqBand_Images[k].transform.localScale);
+        }
     }
 }
